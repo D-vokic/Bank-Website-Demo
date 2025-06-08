b933cf95cd5204fbb191b668443bb1f8beaa1b04
Dusko Vokic <vokic.dusko@gmail.com>
2025-06-08 09:06:07 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,191 @@
+# Interactive README,md
+
+This project is a modern, responsive **demo of a banking website**, featuring:
+
+- **Modal window** for displaying additional information
+- **Smooth scrolling** to page sections
+- **Navigation menu** with hover effects and sticky behavior on scroll
+- **Tabs** to switch content within a section
+- **Lazy loading images** for improved performance
+- **Interactive slider** with navigation buttons and slide indicators
+- **Dark mode** with persistent user preference saved in `localStorage`
+- **Hamburger menu** for mobile devices
+
+![Interactive Bank Website Demo](screenshot.png)
+
+---
+
+## Demo
+
+You can view a live demo of this project [here](#).
+
+---
+
+## Table of Contents
+
+- [Interactive Banking UI](#interactive-banking-ui)
+
+  - [Demo](#demo)
+  - [Table of Contents](#table-of-contents)
+  - [Features](#features)
+  - [Technologies Used](#technologies-used)
+  - [Project Structure](#project-structure)
+  - [Getting Started](#getting-started)
+  - [Prerequisites](#prerequisites)
+  - [How to Run](#how-to-run)
+  - [Usage](#usage)
+  - [Built with](#built-with)
+  - [Contributing / Support](#contributing--support)
+  - [Author](#author)
+  - [License](#license)
+  - [Acknowledgments](#acknowledgments)
+
+  ***
+
+## Features
+
+### Modal
+
+Open and close modal by clicking buttons, overlay, or pressing Escape.
+
+### Navigation
+
+- Sticky navigation bar that stays fixed on top when scrolling.
+- Hover effect that dims inactive links and logo.
+- Smooth scroll on navigation link clicks.
+- Hamburger menu for mobile devices.
+
+### Tabs
+
+Interactive tabs switch visible content without reloading the page.
+
+### Slider
+
+- Navigate slides with arrow buttons or dots below.
+- Highlights active dot corresponding to current slide.
+- Keyboard arrow key support for slide navigation.
+
+### Lazy Loading Images
+
+Images load only when near viewport, improving initial load speed.
+
+### Dark Mode
+
+Toggle between light and dark modes, with preference saved in `localStorage`.
+
+---
+
+## Technologies Used
+
+- HTML5
+- CSS3 (with variables and animations)
+- JavaScript (ES6+)
+- Intersection Observer API
+- Local Storage for saving settings
+- Responsive Web Design
+
+---
+
+## Project Structure
+
+Bank Website Demo/
+├── css/
+├── components/
+└── darkMode.css
+└── hamburger.css
+└── modal.css
+└── scrollToTop.css.css
+└── slider.css.css
+├── responsive/
+└── responsive-480.css
+└── responsive-500.css
+└── responsive-576.css
+└── responsive-768.css
+└── responsive-900.css
+└── responsive-1024.css
+└── responsive-1150.css
+└── responsive-1300.css
+├── sections/
+└── features.css
+└── footer.css
+└── header.css
+└── nav.css
+└── operations.css
+└── signup.css
+└── testimonials.css
+└── base.css
+├── img/ # Images and icons
+├── script.js
+├── .gitignore
+├── .prettierrc
+├── index.html
+├── LICENSE.md
+├── screenshot.png
+└── README.md
+
+---
+
+### Getting Started
+
+These instructions will get you a copy of the project up and running on your local machine for development and testing purposes.
+
+---
+
+### Prerequisites
+
+What things you need to install the software and how to install them:
+
+- A modern web-browser (Chrome, Firefox, Safari, Edge)
+
+---
+
+### How to Run Locally
+
+1. Clone the repository.
+2. Open index.html in your preferred browser or use a live server (e.g., VSCode Live Server extension).
+
+---
+
+## Usage
+
+1. Click "Open Modal" button to show the modal window.
+2. Use navigation links to smoothly scroll between sections.
+3. Click tabs to switch content inside the operations section.
+4. Navigate slider slides using arrows or dots.
+5. dark mode using the icon in the top-right corner.
+6. On mobile, use the hamburger menu to access navigation links.
+
+---
+
+## Built With
+
+- [HTML](https://developer.mozilla.org/en-US/docs/Web/HTML) - The markup language used
+- [CSS](https://developer.mozilla.org/en-US/docs/Web/CSS) - The style sheet language used
+- [JavaScript](https://developer.mozilla.org/en-US/docs/Web/JavaScript) - The programming language used
+
+---
+
+## Contributing / Support
+
+-Contributions are welcome! If you find any issues or have suggestions for improvements, feel free to open an issue or create a pull request.
+-If you're having any problem, please raise an issue on GitHub and the maintainers will help you out.
+
+---
+
+## Author
+
+This project is maintained by Duško Vokić. You can reach out to me on [GitHub](https://github.com/D-vokic?tab=repositories) or [LinkedIn](https://www.linkedin.com/in/du%C5%A1ko-voki%C4%87-0337a2106) for any queries or suggestions.
+
+---
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details
+
+---
+
+## Acknowledgments
+
+- [Font Awesome](https://fontawesome.com/) - Used for icons.
+- [Unsplash](https://unsplash.com/) - Source of album cover images.
+- [GitHub Pages](https://pages.github.com/) - Hosting the project website.
---
+++ script.js
@@ -0,0 +1,372 @@
+'use strict';
+
+/** @type {HTMLElement} */
+const modal = document.querySelector('.modal');
+/** @type {HTMLElement} */
+const overlay = document.querySelector('.overlay');
+/** @type {HTMLElement} */
+const btnCloseModal = document.querySelector('.btn--close-modal');
+/** @type {NodeListOf<HTMLElement>} */
+const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+/** @type {HTMLElement} */
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+/** @type {HTMLElement} */
+const section1 = document.querySelector('#section--1');
+/** @type {HTMLElement} */
+const tabsContainer = document.querySelector('.operations__tab-container');
+/** @type {NodeListOf<HTMLElement>} */
+const tabs = document.querySelectorAll('.operations__tab');
+/** @type {NodeListOf<HTMLElement>} */
+const tabsContent = document.querySelectorAll('.operations__content');
+/** @type {HTMLElement} */
+const nav = document.querySelector('.nav');
+/** @type {HTMLImageElement} */
+const logoImg = document.getElementById('logo');
+/** @type {NodeListOf<HTMLSpanElement>} */
+const allSpans = document.querySelectorAll('.nav__hamburger span');
+/** @type {HTMLElement} */
+const hamburger = document.getElementById('hamburger');
+/** @type {HTMLElement} */
+const navLinks = document.querySelector('.nav__links');
+
+/**
+ * Update the background color of hamburger menu spans depending on dark mode.
+ * @returns {void}
+ */
+function updateHamburgerSpansBorder() {
+  if (document.body.classList.contains('dark-mode')) {
+    allSpans.forEach(span => {
+      span.style.backgroundColor = 'white';
+    });
+  } else {
+    allSpans.forEach(span => {
+      span.style.backgroundColor = '';
+    });
+  }
+}
+
+/** @type {HTMLElement} */
+const header = document.querySelector('.header');
+const navHeight = nav.getBoundingClientRect().height;
+/** @type {NodeListOf<HTMLElement>} */
+const allSections = document.querySelectorAll('.section');
+/** @type {HTMLElement} */
+const scrollToTopBtn = document.getElementById('scrollToTopBtn');
+
+window.addEventListener('scroll', () => {
+  if (window.pageYOffset > 300) {
+    scrollToTopBtn.style.display = 'block';
+  } else {
+    scrollToTopBtn.style.display = 'none';
+  }
+});
+
+scrollToTopBtn.addEventListener('click', () => {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth',
+  });
+});
+
+/** @type {HTMLElement} */
+const modeToggleBtn = document.getElementById('modeToggleBtn');
+
+/**
+ * Open modal window.
+ * @param {Event} e Event object
+ * @returns {void}
+ */
+const openModal = function (e) {
+  e.preventDefault();
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+/**
+ * Close modal window.
+ * @returns {void}
+ */
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+btnsOpenModal.forEach(btn => btn.addEventListener('click', openModal));
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
+
+btnScrollTo.addEventListener('click', function (e) {
+  section1.scrollIntoView({ behavior: 'smooth' });
+});
+
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+  e.preventDefault();
+  if (e.target.classList.contains('nav__link')) {
+    const id = e.target.getAttribute('href');
+    document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+  }
+});
+
+tabsContainer.addEventListener('click', function (e) {
+  const clicked = e.target.closest('.operations__tab');
+  if (!clicked) return;
+
+  tabs.forEach(t => t.classList.remove('operations__tab--active'));
+  tabsContent.forEach(c => c.classList.remove('operations__content--active'));
+
+  clicked.classList.add('operations__tab--active');
+
+  document
+    .querySelector(`.operations__content--${clicked.dataset.tab}`)
+    .classList.add('operations__content--active');
+});
+
+/**
+ * Handles hover effect on navigation links.
+ * @param {MouseEvent} e Mouse event
+ * @returns {void}
+ */
+const handleHover = function (e) {
+  if (e.target.classList.contains('nav__link')) {
+    const link = e.target;
+    const siblings = link.closest('.nav').querySelectorAll('.nav__link');
+    const logo = link.closest('.nav').querySelector('img');
+
+    siblings.forEach(el => {
+      if (el !== link) el.style.opacity = this;
+    });
+    logo.style.opacity = this;
+  }
+};
+
+// Passing "argument" into handler: 0.5 for mouseover, 1 for mouseout
+nav.addEventListener('mouseover', handleHover.bind(0.5));
+nav.addEventListener('mouseout', handleHover.bind(1));
+
+const initialCords = section1.getBoundingClientRect();
+
+window.addEventListener('scroll', function () {
+  if (window.scrollY > initialCords.top) nav.classList.add('sticky');
+  else nav.classList.remove('sticky');
+});
+
+/**
+ * IntersectionObserver callback for sticky navigation.
+ * @param {IntersectionObserverEntry[]} entries Intersection observer entries
+ */
+const stickyNav = function (entries) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) nav.classList.add('sticky');
+  else nav.classList.remove('sticky');
+};
+
+const headerObserver = new IntersectionObserver(stickyNav, {
+  root: null,
+  threshold: 0,
+  rootMargin: `-${navHeight}px`,
+});
+headerObserver.observe(header);
+
+/**
+ * IntersectionObserver callback to reveal sections on scroll.
+ * @param {IntersectionObserverEntry[]} entries Intersection observer entries
+ * @param {IntersectionObserver} observer Observer instance
+ */
+const revealSection = function (entries, observer) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) return;
+
+  entry.target.classList.remove('section--hidden');
+  observer.unobserve(entry.target);
+};
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.1,
+});
+
+allSections.forEach(function (section) {
+  sectionObserver.observe(section);
+});
+
+/** @type {NodeListOf<HTMLImageElement>} */
+const imgTargets = document.querySelectorAll('img[data-src]');
+
+/**
+ * IntersectionObserver callback to lazy-load images.
+ * @param {IntersectionObserverEntry[]} entries Intersection observer entries
+ * @param {IntersectionObserver} observer Observer instance
+ */
+const loadImg = function (entries, observer) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) return;
+
+  entry.target.src = entry.target.dataset.src;
+
+  entry.target.addEventListener('load', function () {
+    entry.target.classList.remove('lazy-img');
+  });
+
+  observer.unobserve(entry.target);
+};
+
+const imgObserver = new IntersectionObserver(loadImg, {
+  root: null,
+  threshold: 0,
+  rootMargin: '-200px',
+});
+
+imgTargets.forEach(img => imgObserver.observe(img));
+
+/**
+ * Slider component initialization function.
+ * Handles slide navigation, dots, and keyboard controls.
+ * @returns {void}
+ */
+const slider = function () {
+  /** @type {NodeListOf<HTMLElement>} */
+  const slides = document.querySelectorAll('.slide');
+  /** @type {HTMLElement} */
+  const btnLeft = document.querySelector('.slider__btn--left');
+  /** @type {HTMLElement} */
+  const btnRight = document.querySelector('.slider__btn--right');
+  /** @type {HTMLElement} */
+  const dotContainer = document.querySelector('.dots');
+
+  let curSlide = 0;
+  const maxSlide = slides.length;
+
+  /**
+   * Creates dot buttons for each slide.
+   * @returns {void}
+   */
+  const createDots = function () {
+    slides.forEach(function (_, i) {
+      dotContainer.insertAdjacentHTML(
+        'beforeend',
+        `<button class="dots__dot" data-slide="${i}"></button>`
+      );
+    });
+  };
+
+  /**
+   * Activates the dot corresponding to the current slide.
+   * @param {number} slide Slide index
+   * @returns {void}
+   */
+  const activateDot = function (slide) {
+    document
+      .querySelectorAll('.dots__dot')
+      .forEach(dot => dot.classList.remove('dots__dot--active'));
+
+    document
+      .querySelector(`.dots__dot[data-slide="${slide}"]`)
+      .classList.add('dots__dot--active');
+  };
+
+  /**
+   * Moves to the specified slide.
+   * @param {number} slide Slide index
+   * @returns {void}
+   */
+  const goToSlide = function (slide) {
+    slides.forEach(
+      (s, i) => (s.style.transform = `translateX(${100 * (i - slide)}%)`)
+    );
+  };
+
+  /**
+   * Goes to the next slide.
+   * @returns {void}
+   */
+  const nextSlide = function () {
+    if (curSlide === maxSlide - 1) {
+      curSlide = 0;
+    } else {
+      curSlide++;
+    }
+    goToSlide(curSlide);
+    activateDot(curSlide);
+  };
+
+  /**
+   * Goes to the previous slide.
+   * @returns {void}
+   */
+  const prevSlide = function () {
+    if (curSlide === 0) {
+      curSlide = maxSlide - 1;
+    } else {
+      curSlide--;
+    }
+    goToSlide(curSlide);
+    activateDot(curSlide);
+  };
+
+  /**
+   * Initializes the slider.
+   * @returns {void}
+   */
+  const init = function () {
+    goToSlide(0);
+    createDots();
+    activateDot(0);
+  };
+  init();
+
+  // Dark mode initialization
+  const savedMode = localStorage.getItem('darkMode');
+  if (savedMode === 'enabled') {
+    document.body.classList.add('dark-mode');
+    modeToggleBtn.textContent = '☀️';
+  }
+
+  modeToggleBtn.addEventListener('click', () => {
+    document.body.classList.toggle('dark-mode');
+    const isDark = document.body.classList.contains('dark-mode');
+
+    if (isDark) {
+      modeToggleBtn.innerHTML = '<i class="fa-solid fa-sun"></i>';
+      logoImg.src = 'img/sohoBela.webp'; // Dark mode logo
+      localStorage.setItem('darkMode', 'enabled');
+    } else {
+      modeToggleBtn.innerHTML = '<i class="fa-solid fa-umbrella-beach"></i>';
+      modeToggleBtn.style.color = 'var(--text-color)';
+      modeToggleBtn.style.backgroundColor = '#ffffff';
+      logoImg.src = 'img/icon.png'; // Light mode logo
+      localStorage.setItem('darkMode', 'disabled');
+    }
+
+    updateHamburgerSpansBorder();
+  });
+
+  hamburger.addEventListener('click', () => {
+    navLinks.classList.toggle('nav__links--active');
+  });
+
+  btnRight.addEventListener('click', nextSlide);
+  btnLeft.addEventListener('click', prevSlide);
+
+  document.addEventListener('keydown', function (e) {
+    if (e.key === 'ArrowLeft') prevSlide();
+    e.key === 'ArrowRight' && nextSlide();
+  });
+
+  dotContainer.addEventListener('click', function (e) {
+    if (e.target.classList.contains('dots__dot')) {
+      curSlide = Number(e.target.dataset.slide);
+      goToSlide(curSlide);
+      activateDot(curSlide);
+    }
+  });
+};
+
+slider();
